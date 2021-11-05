d91f3f632c67ffbbf0ff0a53193d969ea9effd0f
gil <gilmt7912@gmail.com>
2021-11-06 00:08:39 +0900

    fix manifest

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,12 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravue') }}</title>
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
 </head>
 <body>
 <div id="app">
     <app-component></app-component>
 </div>
-<script src="{{ mix('js/app.js') }}" defer></script>
+<script src="{{ mix('/js/app.js') }}" defer></script>
 </body>
 </html>
