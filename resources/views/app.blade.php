f656f05e9db171aeadb116505b92576c21b0bdc8
gil <gilmt7912@gmail.com>
2021-11-06 03:48:12 +0900

    fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravue') }}</title>
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+    <link rel="shortcut icon" href="{{ asset('/favicon.ico') }}">
 </head>
 <body>
 <div id="app">
